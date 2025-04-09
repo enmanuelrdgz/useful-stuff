80923accf2d8ce7dae7313974c46dcd272cc2394
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-04-09 17:18:44 -0300

    add some code to socket

--- socket/Main.java
+++ socket/Main.java
@@ -1,16 +1,28 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+
+// clases involucradas:
+// InputStream
+// InputStreamReader
+// Socket y ServerSocket
 
 public class Main {
     public static void main(String[] args) {
         try {
-            ServerSocket server_socket = new ServerSocket(1234);
-            server_socket.accept();
-            server_socket.close();
+            ServerSocket socket = new ServerSocket();
+            Socket client_socket = socket.accept();
+            InputStreamReader reader = new InputStreamReader(client_socket.getInputStream());
+            // Declarar una variable para almacenar el nombre del metodo
+            // while(reader.read() != 32) do: metodo[i] = reader.read()
+            // convertir metodo a String
+            // asignarlo al nuevo objeto HttpRequest
         } catch(IOException ex) {
-
+            System.out.println(ex.getMessage());
         }
     }
 }
