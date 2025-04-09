79df43ade5e9d37379ed22bfbd571e60e010d3eb
Enmanuel Rodriguez <enmaundostres@gmail.com>
2025-04-09 00:30:50 -0300

    ejemplo JDBC

---
+++ ejemplo_jdbc/Main.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // para establecer una coneccion JDBC se usa el DriverManager        
+        
+        try {
+            Connection conn = DriverManager.getConnection("database_url", "user", "password");
+        } catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
